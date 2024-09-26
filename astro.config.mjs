0583acf8000e750bf31be99efe52a87995dc72f9
Tammy Zhang <tz332@cornell.edu>
2024-09-26 02:25:59 -0400

    config partytown; update post template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
     site: 'https://tammyzhang-1.github.io',
-    base: '/cisl-visualization-gallery'
+    base: '/cisl-visualization-gallery',
+    integrations: [ partytown({
+        config: {
+          forward: ["dataLayer.push"],
+        }
+    })]
 });
