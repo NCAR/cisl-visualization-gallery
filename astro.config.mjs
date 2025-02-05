ad08c19a2da0c2729c9e9ecab5db7ff6d9276da7
Tammy Zhang <tz332@cornell.edu>
2025-02-05 12:57:59 -0700

    adjust google analytics script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown';
+// import partytown from '@astrojs/partytown';
 import pagefind from "astro-pagefind";
 
 export default defineConfig({
     site: 'https://visualizations.ucar.edu',
     base: '/',
-    integrations: [ partytown({
-        config: {
-          forward: ["dataLayer.push"],
-        }
-    }), pagefind()]
+    integrations: [ 
+    //   partytown({
+    //     config: {
+    //       forward: ["dataLayer.push"],
+    //     }
+    // }), 
+    pagefind()]
 });
